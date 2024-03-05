16823ed32b8e17117d0adb1651209fc832621842
Ritusuri <ritukumarihit@gmail.com>
2024-03-05 10:26:33 +0530

    our auth app is ready

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const PageNotFound = () =>{
+    return(
+        <div className="flex items-center min-h-screen justify-center">
+            <h1 className="font-bold text-lg">whoops page not found you are looking for</h1>
+        </div>
+
+    )
+}
+
+export default PageNotFound;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    // Add other file paths containing HTML content
+  ],
+  // Other Tailwind CSS configuration options...
+}
+
+
